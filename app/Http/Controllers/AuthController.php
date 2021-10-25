6a2a4d3ed89cf23e744bf58bb673674ec87f4f6c
kookoo135 <kookoo135@gmail.com>
2021-10-25 13:09:58 +0900

    laravel project - 회원 성별 추가

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,6 +28,7 @@ class AuthController extends Controller
             'password' => bcrypt($request->password),
             'phone' => $request->phone,
             'email' => $request->email,
+            'sex' => $request->sex,
         ]);
 
         $user->save();
